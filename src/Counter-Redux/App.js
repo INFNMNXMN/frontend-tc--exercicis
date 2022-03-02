e8b4204ad14cfadca73d4052dbf03b1be8c62c16
INFNMNXMN <97982668+INFNMNXMN@users.noreply.github.com>
2022-03-02 20:03:11 +0100

    Commit 1

---
+++ src/Counter-Redux/App.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function App() {
+  const counter = {state} => state + 1;
+  const {count, setCount} = useState(0);
+  
+  return (
+    <>
+      <h1>Counter</h1>
+      <div>{count}</div>
+      <button onClick={()=> setCount((state) => state + 1 )}>+1</button>
+    </>
+  );
+}
+
+export default App;
